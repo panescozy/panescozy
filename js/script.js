73c7a2ae41894ec2e05f0549cd79a5dab9b59606
panescozy <164271491+panescozy@users.noreply.github.com>
2024-04-04 00:16:53 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ function addTextToEmptyChildren(parent) {
     var op1 = document.getElementById("o-title");
     var op2 = document.getElementById("o-spec");
     op1.textContent ='Lactal';
-    op2.textContent ='Pan lactal totalmente natural, con un peso de 850gr y 32cm de largo, 10cm de ancho y 10cm de alto.';
+    op2.textContent ='Pan lactal totalmente natural, con un peso de 800gr y 26cm de largo, 10cm de ancho y 10cm de alto.';
 }
 
 function removeTextFromChildren(parent) {
